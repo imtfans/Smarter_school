264c78a92acb511bbd9a47865c470cb1faffd214
godsent <godsentojugo@gmail.com>
2025-12-17 19:38:07 +0100

    Fix healthz URL path

--- smarter_school/smarter_school/urls.py
+++ smarter_school/smarter_school/urls.py
@@ -1,30 +1,11 @@
-"""
-URL configuration for smarter_school project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from core.views import (
     UserViewSet, StudentViewSet, TeacherViewSet,
-    SubjectViewSet, ResultViewSet, AttendanceViewSet
+    SubjectViewSet, ResultViewSet, AttendanceViewSet, healthz
 )
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from core.views import healthz
 
 
 router = DefaultRouter()
@@ -35,6 +16,7 @@ router.register(r'subjects', SubjectViewSet, basename='subject')
 router.register(r'results', ResultViewSet, basename='result')
 router.register(r'attendance', AttendanceViewSet, basename='attendance')
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
@@ -42,6 +24,7 @@ urlpatterns = [
     # JWT authentication endpoints
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    ("healthz", healthz),
-    # or path("healthz/", healthz),
+
+    # Health check
+    path('healthz/', healthz),
 ]
